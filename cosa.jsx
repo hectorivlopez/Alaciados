3158a2374a7f47e818ce5df03b0c81faf507412a
Hector Lopez <hectorivlopez@gmail.com>
2024-07-31 19:13:32 -0600

    Si

---
+++ cosa.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const cosa = () => {
+  return (
+    <View>
+      <Text>cosa</Text>
+    </View>
+  );
+};
+
+export default cosa;
